3db4b68804b3fd8e72b8f7fe15f7555020910853
V <zhangyibin481516@gmail.com>
2026-03-07 13:12:40 +1100

    fix: fix errors

--- backend/run.py
+++ backend/run.py
@@ -2,4 +2,4 @@ from uvicorn import run
 
 if __name__ == "__main__":
     print("科研助手应用已启动")
-    run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    run("app.main:app", host="0.0.0.0", port=8000, reload=True, reload_dirs=["app"])
